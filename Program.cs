f0186c85235b70bacce465869ccfe40b666fd860
BrianBarmao <bryanbarmao@gmail.com>
2023-01-15 22:45:10 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using EventsHubReciever;
+using System.Text;
+using System.Text.Json;
+using static Azure.Core.HttpHeader;
+
+// Create a blob container client that the event processor will use 
+BlobContainerClient storageClient = new BlobContainerClient(
+    "<connection string>", 
+    "<blob storage name>");
+
+// Create an event processor client to process events in the event hub
+var processor = new EventProcessorClient(
+    storageClient,
+    EventHubConsumerClient.DefaultConsumerGroupName,
+     "<connection string>",
+    "<event hub name>");
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+// Start the processing
+await processor.StartProcessingAsync();
+
+// Wait for 30 seconds for the events to be processed
+await Task.Delay(TimeSpan.FromSeconds(10));
+
+// Stop the processing
+await processor.StopProcessingAsync();
+
+async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    // Write the body of the event to the console window
+    Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+    var attribs = JsonSerializer.Deserialize<StandardCommon>(eventArgs.Data.Body.ToString());
+
+    // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+    await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    return Task.CompletedTask;
+}
---
+++ StandardCommon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsHubReciever
+{
+    public class StandardCommon
+    {
+        public string ClientId { get; set; }
+        public long Imei { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string GPSDate { get; set; }
+        public int Heading { get; set; }
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public string AdditionalEventInfo { get; set; }
+        public decimal Odometer { get; set; }
+        public long? EngineHours { get; set; }
+        public decimal VehicleSpeed { get; set; }
+        public string RoadSpeedLimit { get; set; }
+        public string RoadName { get; set; }
+        public string GeoZoneID { get; set; }
+        public int DriverID { get; set; }
+        public bool IsIgnitionOn { get; set; }
+        public string Altitude { get; set; }
+        public string SensorData { get; set; }  //analog, 1-wire, serial
+        public string CanBusData { get; set; }  //fuel etc
+        public string Attributes { get; set; }  //Number Of Sats, GSM Strength, Car Battery power, internal tracker battery power
+        public string CellTowerInfo { get; set; } //MMC, MNC, LAC, ID        
+        public string RawData { get; set; }
+        public string AssetName { get; set; }
+    }
+}
